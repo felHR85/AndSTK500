ac3ebfb7781504f072de50ce0d80bd627f5d79ae
Felipe Herranz <felhr85@gmail.com>
2016-05-11 22:53:13 +0200

    STKReadSignature added

---
+++ andstk500/src/main/java/felhr/com/andstk500/commands/STKReadSignature.java
@@ -0,0 +1,23 @@
+package felhr.com.andstk500.commands;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Read signature bytes.
+ */
+public class STKReadSignature extends STK500Command
+{
+    public STKReadSignature()
+    {
+        super(STK500Constants.Cmnd_STK_READ_SIGN, 2);
+    }
+
+    @Override
+    public byte[] getCommandBuffer()
+    {
+        ByteBuffer buffer = ByteBuffer.allocate(2);
+        buffer.put((byte) STK500Constants.Cmnd_STK_READ_SIGN);
+        buffer.put((byte) STK500Constants.Sync_CRC_EOP);
+        return buffer.array();
+    }
+}
