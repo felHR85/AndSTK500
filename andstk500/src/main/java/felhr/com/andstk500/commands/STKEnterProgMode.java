4bc7ce8a1793ea16ecf3a36c36e42a0a899a0a03
Felipe Herranz <felhr85@gmail.com>
2016-05-11 22:45:16 +0200

    STKEnterProgMode added

---
+++ andstk500/src/main/java/felhr/com/andstk500/commands/STKEnterProgMode.java
@@ -0,0 +1,26 @@
+package felhr.com.andstk500.commands;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Enter Programming mode for the selected device. The Programming mode and device
+ *programming parameters must have been set by Cmnd_STK_SET_DEVICE prior to
+ *calling this command, or the command will fail with a Resp_STK_NODEVICE response.
+ */
+
+public class STKEnterProgMode extends STK500Command
+{
+    public STKEnterProgMode()
+    {
+        super(STK500Constants.Cmnd_STK_ENTER_PROGMODE, 2);
+    }
+
+    @Override
+    public byte[] getCommandBuffer()
+    {
+        ByteBuffer buffer = ByteBuffer.allocate(2);
+        buffer.put((byte) STK500Constants.Cmnd_STK_ENTER_PROGMODE);
+        buffer.put((byte) STK500Constants.Sync_CRC_EOP);
+        return buffer.array();
+    }
+}
