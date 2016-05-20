ec6d00564e516f09fcaa9849370f067b285080ee
Felipe Herranz <felhr85@gmail.com>
2016-05-20 23:47:25 +0200

    STKNosync response added

--- andstk500/src/main/java/felhr/com/andstk500/responses/STK500ResponseGenerator.java
+++ andstk500/src/main/java/felhr/com/andstk500/responses/STK500ResponseGenerator.java
@@ -1,21 +1,45 @@
 package felhr.com.andstk500.responses;
 
+import java.util.Arrays;
+
+import felhr.com.andstk500.commands.STK500Constants;
+
 /**
  * Class for generate STK500 responses
  */
 public class STK500ResponseGenerator
 {
+    private static final int MAX_BUFFER = 256 + 5; // Max command length
+
+    private byte[] responseBuffer;
+    private int pointer;
+
+    private boolean newCommand;
+
     private STK500Response currentResponse;
+
 
     public STK500ResponseGenerator()
     {
-
+        this.responseBuffer = new byte[MAX_BUFFER];
+        this.pointer = 0;
+        this.newCommand = true;
     }
 
     public boolean generateSTK500Response(int commandType, byte[] buffer)
     {
         //TODO: Generate correct STK500 responses classes for each command.
         //TODO: Return true if the response is correct. False if some bytes are missing
+        if(newCommand)
+        {
+            if(commandType == STK500Constants.Cmnd_STK_GET_SIGN_ON)
+            {
+                return generateSTKGetSignOnResponse(buffer);
+            }
+        }else
+        {
+
+        }
         return false;
     }
 
@@ -25,4 +49,22 @@ public class STK500ResponseGenerator
         currentResponse = null;
         return response;
     }
+
+    private boolean generateSTKGetSignOnResponse(byte[] buffer)
+    {
+        int maxResponseLength = 3;
+        if(buffer.length == maxResponseLength)
+        {
+            currentResponse = new STKInsync(
+                    STK500Constants.Cmnd_STK_GET_SIGN_ON, new int[]{buffer[1]}, null, true);
+            return true;
+        }else
+        {
+            System.arraycopy(buffer, 0, responseBuffer, pointer, buffer.length);
+            pointer = buffer.length;
+            return false;
+        }
+    }
+
+
 }
---
+++ andstk500/src/main/java/felhr/com/andstk500/responses/STKNosync.java
@@ -0,0 +1,12 @@
+package felhr.com.andstk500.responses;
+
+/**
+ * STKNosync response
+ */
+public class STKNosync extends STK500Response
+{
+    public STKNosync()
+    {
+
+    }
+}
