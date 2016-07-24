20f9e14814591b9241380a42c698fd8fcad9c62f
Felipe Herranz <felhr85@gmail.com>
2016-07-24 11:49:21 +0200

    added IntelHexParser and ArduinoSketch classes

---
+++ andstk500/src/main/java/felhr/com/andstk500/arduino/ArduinoSketch.java
@@ -0,0 +1,8 @@
+package felhr.com.andstk500.arduino;
+
+/**
+ * An Arduino sketch in binary format
+ */
+public class ArduinoSketch
+{
+}
---
+++ andstk500/src/main/java/felhr/com/andstk500/arduino/IntelHexParser.java
@@ -0,0 +1,17 @@
+package felhr.com.andstk500.arduino;
+
+import java.io.File;
+
+/**
+ * Intel Hex parser
+ * For more info: https://en.wikipedia.org/wiki/Intel_HEX
+ */
+public class IntelHexParser
+{
+    protected static ArduinoSketch parseHexFile(File file)
+    {
+        //TODO:
+        return null;
+    }
+
+}
