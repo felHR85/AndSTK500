0cd1ab46c4116f6df07e4edaa6bb457e58a2b016
Felipe Herranz <felhr85@gmail.com>
2016-07-25 12:53:11 +0200

    ArduinoSketch class implemented

--- andstk500/src/main/java/felhr/com/andstk500/arduino/ArduinoSketch.java
+++ andstk500/src/main/java/felhr/com/andstk500/arduino/ArduinoSketch.java
@@ -1,8 +1,40 @@
 package felhr.com.andstk500.arduino;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * An Arduino sketch in binary format
  */
 public class ArduinoSketch
 {
+    private List<byte[]> data;
+    private List<Integer> addresses;
+
+    public ArduinoSketch()
+    {
+        data = new ArrayList<>();
+        addresses = new ArrayList<>();
+    }
+
+    public void putBuffer(int offset, byte[] buffer)
+    {
+        data.add(buffer);
+        addresses.add(offset);
+    }
+
+    public int getCount()
+    {
+        return data.size();
+    }
+
+    public Map<Integer, byte[]> getData(int index)
+    {
+        Map<Integer, byte[]> mappedData = new HashMap<>();
+        mappedData.put(addresses.get(index), data.get(index));
+        return mappedData;
+    }
+
 }
